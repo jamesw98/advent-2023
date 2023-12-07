7862faed826df71a450f8cad7cc9a149fb6261d0
jamesw98 <xeekatar@gmail.com>
2023-12-06 22:44:32 -0500

    added template

---
+++ Template.cs
@@ -0,0 +1,40 @@
+﻿namespace advent_2023;
+
+public class Template : Day
+{
+    public Template(string filename) : base(filename)
+    {
+    }
+
+    public void Solve()
+    {
+        Console.WriteLine(Part1());
+        Console.WriteLine(Part2());
+    }
+
+    private long Part1()
+    {
+        // var result = 1;
+        var result = 0; 
+        
+        Lines.Each((line, index) =>
+        {
+            
+        });
+        
+        return result;
+    }
+
+    private long Part2()
+    {
+        // var result = 1;
+        var result = 0; 
+        
+        Lines.Each((line, index) =>
+        {
+            
+        });
+        
+        return result;
+    }
+}
